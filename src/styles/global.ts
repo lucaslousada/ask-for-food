e88ec22426e002224bdfd41938885e71ea22b6be
Lucas Lousada <lucas_lousada@outlook.com>
2022-06-06 14:47:50 -0300

    font weight change

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,13 +18,16 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     background: ${({ theme }) => theme.colors.color_400};
-    font-weight: 500;
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
     font-family: 'Gordita', sans-serif;
     color: ${({ theme }) => theme.colors.color_900};
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
   }
 
   button {
